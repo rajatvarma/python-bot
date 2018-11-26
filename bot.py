942b33d540118aa14467a444e8396a97bffa8743
Rajat Varma <rajatvarma08@gmail.com>
2018-11-26 19:29:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
 import random
 import discord
+
 from discord.ext.commands import Bot
 
 BOT_PREFIX = "^"
 TOKEN = 'NDkwNDQyNDg3ODQ0ODMxMjQ5.Dn5YHw.MW82PBMnAxm3QnhCON3-Q4WB_jA'
 
 client = Bot(command_prefix=BOT_PREFIX)
-
-muted = []
-
 
 @client.event
 async def on_message(message):
@@ -18,34 +16,38 @@ async def on_message(message):
     if message.content.startswith(BOT_PREFIX + 'hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
+
     if message.content.startswith(BOT_PREFIX + 'eightball'):
         responses = ["Yes", "No", "Don't count on it...", "Chances are high...", "50/50, I'd say"]
         msg = (random.choice(responses) + ', ' + '{0.author.mention}!').format(message)
         await client.send_message(message.channel, msg)
+
     if message.content.startswith(BOT_PREFIX + 'ileftfortnite'):
         msg = 'Wow {0.author.mention}! You increased your chances of getting laid by 1%!'.format(message)
         await client.send_message(message.channel, msg)
+
     if message.content.startswith(BOT_PREFIX + 'rank'):
         msg = 'Sorry, but ranking other than MEE6 is still under development!'
         await client.send_message(message.channel, msg)
+
     if message.content.startswith(BOT_PREFIX + 'help'):
         msg = '''
         The commands available are:
-            `!hello` : Greets the user
-            `!ileftfortnite` : Well, try it
-            `!rank` : Please don't try it now
-            `!eightball` : Answers a simple Yes/No question
-        '''
+            `%shello` : Greets the user
+            `%sileftfortnite` : Well, try it
+            `%srank` : Please don't try it now
+            `%seightball` : Answers a simple Yes/No question
+        '''.format(BOT_PREFIX)
         await client.send_message(message.channel, msg)
-    if "SMUGFACE" in message.content.upper() or "SMUGASS" in message.content.upper():
-        await client.delete_message(message)
-        await client.send_message(message.channel, "No u, fucking cunt!")
+
     if message.content.startswith(BOT_PREFIX + 'mute'):
         muted = message.raw_mentions
         await client.add_roles(muted, discord.utils.get(message.server.roles, name='Muted'))
+
     if message.content.startswith(BOT_PREFIX + 'unmute'):
         unmuted = message.raw_mentions
         await client.remove_roles(unmted, discord.utils.get(message.server.roles, name='Muted'))
+
     if message.content.startswith(BOT_PREFIX + 'autism'):
         cont = message.content.split(" ")
         x = ""
@@ -58,8 +60,15 @@ async def on_message(message):
                 x += char.upper()
             i += 1
         await client.send_message(message.channel, x)
-    if message.content.upper() == "ALEXA, PLAY DESPACITO":
-        await client.send_message(message.channel, '''Despacito Quiero respirar tu cuello despacito Deja que te diga cosas al oído Para que te acuerdes si no estás conmigo Despacito Quiero desnudarte a besos despacito Firmo en las paredes de tu laberinto''', tts=True)
+
+    if message.content.upper() == "ALEXA, PLAY DESPACITO" or message.content.upper() == "ALEXA PLAY DESPACITO":
+        await client.send_message(message.channel, 'https://www.youtube.com/watch?v=kJQP7kiw5Fk')
+        await client.send_message(message.channel, 'Want me to sing?')
+        if 'yes' in message.content.upper():
+            await client.send_message(message.channel, '''Despacito Quiero respirar tu cuello despacito Deja que te diga cosas al oído Para que te acuerdes si no estás conmigo Despacito Quiero desnudarte a besos despacito Firmo en las paredes de tu laberinto''', tts=True)
+        else:
+        await client.send_message(message.channel, 'Suit yourself')            
+
 
 @client.event
 async def on_ready():
