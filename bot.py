bbbd41ecaa6cadd0324b693223cc1ffc8bebd6c8
Rajat Varma <rajatvarma08@gmail.com>
2019-11-20 20:04:23 +0530

    fixed some bugs

--- bot.py
+++ bot.py
@@ -18,9 +18,9 @@ conn = psycopg2.connect(dbname="node", user="postgres", password="123456")
 cur = conn.cursor()
 
 def retrieve_ranks(person):
-    if person == "none":
+    if person == "":
         cur.execute("SELECT name, awards_won FROM members ORDER BY awards_won DESC")
-        a = cur.fetchall()
+        a = cur.fetchall()    
     else:
         cur.execute("SELECT name, awards_won FROM members WHERE name='%s' ORDER BY awards_won DESC" % person)
         a = cur.fetchall()
@@ -29,14 +29,22 @@ def retrieve_ranks(person):
 def bump(person):
     score = retrieve_ranks(person)[0][1] + 1
     cur.execute("UPDATE members SET awards_won = %d WHERE name='%s'" % (score, person))
+    conn.commit()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
 
 @client.event
 async def on_message(message):
     if message.content.startswith(BOT_PREFIX + 'hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await message.channel.send(msg)
-        #client.add_roles(, discord.utils.get(message.Server.roles, name="God"))
-
 
     if message.content.startswith(BOT_PREFIX + 'eightball'):
         responses = ["Yes", "No", "Don't count on it...", "Chances are high...", "50/50, I'd say"]
@@ -49,15 +57,19 @@ async def on_message(message):
     if message.content.startswith(BOT_PREFIX + 'bump'):
         person = message.mentions[0].nick
         bump(person)
+        god = retrieve_ranks("")
+        for guild in client.guilds:
+            for member in guild.members:
+                for role in member.roles:
+                    if role.name == "God":
+                        await member.remove_roles(get(member.guild.roles, name="God"))
+                if member.nick == god[0][0]:
+                    god_member = member
+                    role = get(god_member.guild.roles, name="God")
+                    await god_member.add_roles(role)
         await message.channel.send(person + " has been bumped")
-    if message.content.startswith(BOT_PREFIX + 'ur'):
-        for member in guild.members:
-            if member.nick == god[0][0]:
-                god_member = member
-                role = get(god_member.guild.roles, name="God")
-                await god_member.add_roles(role)
     if message.content.startswith(BOT_PREFIX + 'rank'):
-        god = retrieve_ranks("none")
+        god = retrieve_ranks("")
         msg = ""
         for guild in client.guilds:
             godman = god[0][0]
@@ -65,10 +77,6 @@ async def on_message(message):
                 for i in god:
                     if i[0] == member.nick:
                         msg+=  member.name + " : " + str(i[1]) + "\n"
-                if member.nick == god[0][0]:
-                    god_member = member
-                    role = get(god_member.guild.roles, name="God")
-                    await god_member.add_roles(role)
         await message.channel.send(msg)
 
     if message.content.startswith(BOT_PREFIX + 'help'):
@@ -77,9 +85,9 @@ async def on_message(message):
             `%shello` : Greets the user
             `%sileftfortnite` : Well, try it
             `%sbump @person`: Increases a person's rank
-            `%srank` : Please don't try it now
+            `%srank` : Everyone's ranks
             `%seightball` : Answers a simple Yes/No question
-        ''' % BOT_PREFIX, BOT_PREFIX, BOT_PREFIX, BOT_PREFIX)
+        ''' % (BOT_PREFIX, BOT_PREFIX, BOT_PREFIX, BOT_PREFIX, BOT_PREFIX))
         await message.channel.send(msg)
 
     if message.content.startswith(BOT_PREFIX + 'autism'):
@@ -101,10 +109,4 @@ async def on_message(message):
         if 'YES' in message.content.upper():
             await message.channel.send('''Despacito Quiero respirar tu cuello despacito Deja que te diga cosas al oído Para que te acuerdes si no estás conmigo Despacito Quiero desnudarte a besos despacito Firmo en las paredes de tu laberinto''', tts=True)
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
 client.run(TOKEN)   
